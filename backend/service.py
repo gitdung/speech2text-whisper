26c6c577c06680c9f08d9ae75952976fa9ff1e9e
gitdung <andrew.td2003@gmail.com>
2024-11-03 19:12:45 +0700

    Update service.py

--- backend/service.py
+++ backend/service.py
@@ -8,17 +8,18 @@ model = WhisperForConditionalGeneration.from_pretrained("vinai/PhoWhisper-base")
 
 def recognition_service(media):
     try:
+        print(f"con cac: {media}")
         array, sample_rate = torchaudio.load(media)
-        print(array.shape)
+        print(array.shape, 'haha', sample_rate)
         
         if sample_rate > 16000:
-            array = torchaudio.transforms.Resample( sample_rate, 16000)(array)
-        if array.size(dim=0) >1:
+            array = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)(array)
+        if array.size(dim=0) > 1:
             array = array.mean(dim=0)
 
         array = array.squeeze(dim=0)
 
-        input_features = processor(array, sampling_rate=sample_rate, return_tensors="pt").input_features
+        input_features = processor(array, sampling_rate=16000, return_tensors="pt").input_features
 
         predicted_ids = model.generate(input_features)
         transcription = processor.batch_decode(predicted_ids, skip_special_tokens=False)
